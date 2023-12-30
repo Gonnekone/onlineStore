ac0d1b92b8de540d5a47977ba7de4b8561cd7672
Kirill Gushevatyy <opacha2018@yandex.ru>
2023-12-30 15:22:20 +0300

    add routes file

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/Gonnekone/onlineStore/handlers"
 	"github.com/Gonnekone/onlineStore/initializers"
-	"github.com/Gonnekone/onlineStore/middleware"
-	"github.com/gin-gonic/gin"
 )
 
 func init() {
@@ -14,46 +11,7 @@ func init() {
 }
 
 func main() {
-	router := gin.Default()
-	router.Static("/static", "./static")
-
-	api := router.Group("/api")
-
-	// user
-	userControllers := api.Group("/user")
-	{
-		userControllers.POST("/registration", handlers.Registration)
-		userControllers.POST("/login", handlers.Login)
-		userControllers.GET("/auth", middleware.Auth, handlers.Validate)
-	}
-
-	// type
-	typeControllers := api.Group("/type")
-	{
-		typeControllers.POST("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.CreateType)
-		typeControllers.GET("/", handlers.ReadAllTypes)
-		typeControllers.PUT("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.UpdateType)
-		typeControllers.DELETE("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.DeleteType)
-	}
-
-	// brand
-	brandControllers := api.Group("/brand")
-	{
-		brandControllers.POST("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.CreateBrand)
-		brandControllers.GET("/", handlers.ReadAllBrands)
-		brandControllers.PUT("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.UpdateBrand)
-		brandControllers.DELETE("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.DeleteBrand)
-	}
-
-	// device
-	deviceControllers := api.Group("/device")
-	{
-		deviceControllers.POST("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.CreateDevice)
-		deviceControllers.GET("/", handlers.ReadAllDevices)
-		deviceControllers.PUT("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.UpdateDevice)
-		deviceControllers.DELETE("/", middleware.Auth, middleware.CheckRoleAdmin, handlers.DeleteDevice)
-		deviceControllers.GET("/:id", handlers.ReadOneDevice)
-	}
+	router := InitRoutes()
 
 	if err := router.Run(); err != nil {
 		fmt.Println("Failed to start the server")
